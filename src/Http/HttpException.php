9d539ace777147d6e1f9abaae47f28433504e597
Joshua Parker <joshua@joshuaparker.dev>
2025-11-12 10:21:47 -0800

    Removed final methods from contract.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Http/HttpException.php
+++ src/Http/HttpException.php
@@ -25,55 +25,6 @@ interface HttpException
     public function __toString();
 
     /**
-     * Return the exception message.
-     *
-     * @return string
-     */
-    public function getMessage(): string;
-
-    /**
-     * Return the previous exception.
-     *
-     * @return Throwable
-     */
-    public function getPrevious(): Throwable;
-
-    /**
-     * Return the exception code.
-     *
-     * @return mixed
-     */
-    public function getCode(): mixed;
-
-    /**
-     * Return the file in which the exception was created.
-     *
-     * @return string
-     */
-    public function getFile(): string;
-
-    /**
-     * Return the line in which the exception was created.
-     *
-     * @return int
-     */
-    public function getLine(): int;
-
-    /**
-     * Return the stack trace.
-     *
-     * @return array
-     */
-    public function getTrace(): array;
-
-    /**
-     * Return the stack trace as a string.
-     *
-     * @return string
-     */
-    public function getTraceAsString(): string;
-
-    /**
      * Return the status code.
      */
     public function getStatusCode(): int;
