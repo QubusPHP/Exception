2ba6f7b7778294cea03bd0f4e4446c41952de064
Joshua Parker <joshua@joshuaparker.dev>
2025-11-12 22:09:18 -0800

    Removed header comment block.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Exception.php
+++ src/Exception.php
@@ -8,16 +8,6 @@ use Throwable;
 
 use function sprintf;
 
-/**
- * Qubus Exception Class
- *
- * This extends the default `BaseException` class to allow converting
- * exceptions to and from `Error` objects.
- *
- * Unfortunately, because an `Error` object may contain multiple messages and error
- * codes, only the first message for the first error code in the instance will be
- * accessible through the exception's methods.
- */
 class Exception extends \Exception
 {
     /**
